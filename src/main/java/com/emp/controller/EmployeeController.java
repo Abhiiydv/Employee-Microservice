bc992a1d6b50fa9eaace237c1d761292872cd756
unknown <cogjava3120@iiht.tech>
2022-08-18 12:54:51 +0530

    added restTemplate for communication between record and employee

--- src/main/java/com/emp/controller/EmployeeController.java
+++ src/main/java/com/emp/controller/EmployeeController.java
@@ -1,10 +1,13 @@
 package com.emp.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.emp.entity.Employee;
 import com.emp.service.IEmployeeService;
@@ -17,12 +20,17 @@ public class EmployeeController {
 	@Autowired
 	private  IEmployeeService employeeService;
 	
+	@Autowired
+	private RestTemplate restTemplate;
 	
 	@GetMapping("/{eId}")
 	public Employee getEmployee(@PathVariable("eId") Long eId) {
 
-		
 		Employee emp = this.employeeService.getEmployee(eId);
-		return emp;
+		//http://localhost:9090/record/emp/1314
+		List records = this.restTemplate.getForObject("http://localhost:9090/record/emp/" +eId, List.class);
+		
+	   emp.setRecords(records);
+	   return emp;
 }
 }
